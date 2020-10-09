ee4e1a58d837935584ff7fb92d9cf9cd680dc9d1
gayabozu5963 <forestone.5963@gmail.com>
2020-10-09 10:59:14 +0900

    validate error message

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,15 +8,24 @@
 
 @section('content')
    <p>{{$msg}}</p>
+   @if (count($errors) > 0)
+   <div>
+       <ul> 
+           @foreach ($errors->all() as $error)
+               <li>{{ $error }}</li>
+           @endforeach
+       </ul>
+   </div>
+   @endif
    <form action="/hello" method="post">
    <table>
         @csrf
         <tr><th>name: </th><td><input type="text"
-              name="name"></td></tr>
+              name="name" value="{{old('name')}}"></td></tr>
         <tr><th>mail: </th><td><input type="text"
-              name="mail"></td></tr>
+              name="mail" value="{{old('mail')}}"></td></tr>
         <tr><th>age: </th><td><input type="text"
-              name="age"></td></tr>
+              name="age" value="{{old('age')}}"></td></tr>
         <tr><th></th><td><input type="submit"
               value="send"></td></tr>
    </table>

551c5b70a263b2754bc2613b6cd751cadcc69b11
gayabozu5963 <forestone.5963@gmail.com>
2020-10-29 14:35:50 +0900

    pagination

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,13 +7,14 @@ use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
 use Validator;
 use Illuminate\Support\Facades\DB;
+use App\Person;
 
 class HelloController extends Controller
 {
     public function index(Request $request) 
     {
-        $items = DB::table('people')->orderBy('age', 'asc')->get();
-        return view('hello.index', ['items'=> $items]);
+        $items = DB::table('people')->simplePaginate(5);
+        return view('hello.index', ['items' => $items]);
     }
 
     public function post(Request $request)

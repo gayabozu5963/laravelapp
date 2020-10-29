f0d8601362c915ae3ba75a58d395ca895a741c67
gayabozu5963 <forestone.5963@gmail.com>
2020-10-29 11:19:57 +0900

    hello/rest

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -81,6 +81,11 @@ class HelloController extends Controller
              ->get();
         return view('hello.show', ['items' => $items]);
     }
+
+    public function rest(Request $request)
+    {
+        return view('hello.rest');
+    }
 }
 
 
